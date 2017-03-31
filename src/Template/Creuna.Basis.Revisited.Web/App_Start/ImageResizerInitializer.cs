71a74b66e5ef9bac473cd7be557af68fc2e46f9e
Christian Hochlin <christian.hochlin@creuna.no>
2017-03-31 15:31:04 +0200

    Added new image resizer

---
+++ src/Template/Creuna.Basis.Revisited.Web/App_Start/ImageResizerInitializer.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace Creuna.Basis.Revisited.Web.App_Start
+{
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    [InitializableModule]
+    public class ImageResizerInitializer : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {}
+    }
+}
